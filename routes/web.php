3a17c59f456d81357cde8689d306e48f02de8cfc
Roman <romanmironov056@gmail.com>
2025-11-16 20:47:03 -0800

    Monshine

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/register', [AuthController::class, 'showRegister'])->name('register
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
